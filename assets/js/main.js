e98dba247d362627ae8c75602c5da71b8dd80ae9
angelacaterina <angela.caterina@icloud.com>
2020-11-28 16:07:08 +0100

    avanti e indietro con i tasti
    inserita funzione per spostare le immagini con i tasti arrow left e arrow right della tastiera

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,35 @@
 // Potete usare le immagini che desiderate.
 // Per facilitarvi la vita usate immagini delle stesse dimensioni :)
 
-// Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
+// 1. Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider
 // Utiliziamo una classe first e last  per capire quali sono la prima e ultima immagine dello slider
 // Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider
+// 2. In oltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera
 
 $(function() {
-  //mando in avanti le immagini
+  //mando in avanti le immagini (Icone)
   $('.fa-angle-right').click(function () {
     nextImages();
   });
 
-  //mando indietro le immagini
+  //mando indietro le immagini (Icone)
   $('.fa-angle-left').click(function () {
     prevImages();
   });
+
+  //mando avanti e indietro le immagini (tasti)
+  // L'evento onkeydown si verifica quando l'utente preme un tasto
+  onkeydown = function(e) {
+    switch (e.keyCode) {
+      case 37: //arrow left
+        prevImages();
+        break;
+      case 39: //arrow right
+        nextImages();
+        break;
+    }
+  };
+
 });
 
 function nextImages(){
@@ -54,5 +69,3 @@ function prevImages(){
     iconActive.prev('i').addClass('active');
   }
 }
-
-// In oltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera
